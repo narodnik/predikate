e9fc60e397c88485bab63a265db6a8d16103f440
narodnik <x@x>
2020-05-13 09:00:41 +0200

    c to show hide overlayed plots

--- moving_average.py
+++ moving_average.py
@@ -73,18 +73,22 @@ quotes = zip(mdates.date2num(dfn.index.to_pydatetime()),
              dfn.open_adj, dfn.high, dfn.low, dfn.close)
 candlestick_ohlc(ax1, quotes, width=0.01, colorup='g')
 
-ax1.plot(df.index, df["ema-5hr"], lw=3, alpha=0.8,
-         label="EMA (Exponential Moving Average) - 5 hours")
-ax1.plot(df.index, df["ema-1day"], lw=3, alpha=0.8,
-         label="EMA 1 day")
+ema_fast_plot = ax1.plot(
+    df.index, df["ema-5hr"], lw=3, alpha=0.8,
+    label="EMA (Exponential Moving Average) - 5 hours")
+ema_slow_plot = ax1.plot(
+    df.index, df["ema-1day"], lw=3, alpha=0.8,
+    label="EMA 1 day")
 
-ax1.plot(df.loc[df["position"] == 1.0].index, 
-         df["ema-5hr"][df["position"] == 1.0],
-         '^', markersize=10, color='r', label="Buy signal")
+buy_signal_plot = ax1.plot(
+    df.loc[df["position"] == 1.0].index, 
+    df["ema-5hr"][df["position"] == 1.0],
+    '^', markersize=10, color='r', label="Buy signal")
          
-ax1.plot(df.loc[df["position"] == -1.0].index, 
-         df["ema-5hr"][df["position"] == -1.0],
-         'v', markersize=10, color='#555555', label="Sell signal")
+sell_signal_plot = ax1.plot(
+    df.loc[df["position"] == -1.0].index, 
+    df["ema-5hr"][df["position"] == -1.0],
+    'v', markersize=10, color='#555555', label="Sell signal")
 
 last_price_text = "Last price: %1.2f" % df.iloc[-1].close
 ax1.text(0.7, 0.86, last_price_text, transform=ax1.transAxes)
@@ -104,7 +108,8 @@ fig.canvas.mpl_connect('motion_notify_event', snap_cursor.mouse_move)
 
 vol = df.resample('60min').agg({'volume': 'sum'})
 ax1_twin = ax1.twinx()
-ax1_twin.bar(vol.index, vol["volume"], width=0.025, alpha=0.4, zorder=1)
+volume_chart = ax1_twin.bar(vol.index, vol["volume"], width=0.025,
+                            alpha=0.4, zorder=1, label="Volume")
 ax1_twin.set_zorder(1)
 ax1_twin.get_yaxis().set_major_formatter(
     matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
@@ -119,8 +124,22 @@ ax2.set_title("MACD indicator")
 ax2.bar(macd.index, macd, width=0.01)
 
 #plt.tick_params(labelsize=12)
-fig.legend(loc='upper left', fontsize=12)
-#plt.savefig("images/macd.png")
+
+plots_shown = True
+def toggle_plot(event):
+    if event.key != "c":
+        return
+    global plots_shown
+    plots_shown = not plots_shown
+    ema_fast_plot[0].set_visible(plots_shown)
+    ema_slow_plot[0].set_visible(plots_shown)
+    buy_signal_plot[0].set_visible(plots_shown)
+    sell_signal_plot[0].set_visible(plots_shown)
+    for patch in volume_chart.patches:
+        patch.set_visible(plots_shown)
+    fig.canvas.draw()
+
+fig.canvas.mpl_connect('key_press_event', toggle_plot)
 
 mng = plt.get_current_fig_manager()
 mng.window.showMaximized()
