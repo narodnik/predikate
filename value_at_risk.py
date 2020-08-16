40b1f259ebdf98562b52e51dfe783df75a13613f
narodnik <x@x.org>
2020-08-16 19:41:51 +0200

    add figures to VaR

--- value_at_risk.py
+++ value_at_risk.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import math
 import numpy as np
+import scipy.stats as st
 from pull_data import n_days_ago, n_hours_ago, pull
 
 days_ago = 30
@@ -27,4 +28,18 @@ print("BTC variance (1 month):", df.btc_logret.var() * minutes_in_a_month)
 print("ETH variance (1 month):", df.eth_logret.var() * minutes_in_a_month)
 print("Covariance (1 month):")
 print(df.cov() * minutes_in_a_month)
+
+z_score = st.norm.ppf(.95)
+print("z score:", z_score)
+
+btc_interval = z_score * math.sqrt(df.btc_logret.var() * minutes_in_a_month)
+print("+/- z sigma:", btc_interval)
+
+current_price = 11850
+lower_bound = current_price * math.e**-btc_interval
+print("lower bound:", lower_bound)
+
+monthly_logreturn = df.btc_logret.mean() * minutes_in_a_month
+next_avg_price = current_price * math.e**monthly_logreturn
+print("price target:", next_avg_price)
 
