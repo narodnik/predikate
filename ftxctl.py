8003e04697d19f1d20d877c7097af354e0827f0d
narodnik <x@x.org>
2020-11-19 11:04:40 +0100

    improve risk calc

--- ftxctl.py
+++ ftxctl.py
@@ -32,7 +32,8 @@ for position in exchange.fetch_positions():
     ticker = exchange.fetch_ticker(id)
     last = ticker["last"]
     print("  last price:\t", last)
-    print("  risk:\t %.2f%%" % (100 * (last - liquidation) / liquidation))
+    print("  risk:\t %.2f%%" % (
+        100 * (1 - ((last - liquidation) / liquidation))))
 
 def show_btc_markets(exchange):
     print("BTC markets:")
