4b0e93bfd559da85a9b33e382d953366fd09e6f5
narodnik <s>
2021-12-14 21:19:38 +0100

    step

--- bet-sizing/step.py
+++ bet-sizing/step.py
@@ -1,12 +1,7 @@
 import math
 import numpy as np
 
-import stats
-
-def step(current_price, mean, var):
-    # Look ahead 5 hours
-    delta_t = 5
-    
+def step(current_price, mean, var, delta_t):
     # Expected price: P e^(mu T)
     drift = mean * delta_t
     
@@ -20,9 +15,13 @@ def step(current_price, mean, var):
     return next_price
 
 if __name__ == "__main__":
+    import stats
+
+    # Look ahead 5 hours
+    delta_t = 5
     # Mean and variance of lognormal price returns: log (P_i / P_{i - 1})
     mean, var = stats.stats("bitcoin")
     current_price = 46682
-    next_price = step(current_price, mean, var)
+    next_price = step(current_price, mean, var, delta_t)
     print(f"Next price: {next_price}")
 
